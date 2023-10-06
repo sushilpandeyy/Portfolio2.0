8390aa666d2efd697918d9ccd35042badcdb8c87
sushilpandeyy <contact.sushilpandey@gmail.com>
2023-10-06 18:13:52 +0530

    Completed Router Work

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Header from "./component/Header"
 import Footer from './component/Footer'
 import Layout from './component/Layout'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
  
   return (
     <>
       <Header/>
-      <Layout/>
+      <Outlet/>
       <Footer/>
     </>
   )
