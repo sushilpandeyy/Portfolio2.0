e96d4b3b3c9c3462f7c6066bb66ef551ad40668d
sushilpandeyy <contact.sushilpandey@gmail.com>
2023-10-07 16:22:21 +0530

    Angles improved

--- src/component/Connect.jsx
+++ src/component/Connect.jsx
@@ -6,24 +6,25 @@ const Connect = () => {
    <div className="box-atom">
     <div className="allring">
     <div className="nucleus1">
-      <center><img className="img-prop" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png?20200512141346" alt="" /></center>
-      <center><img  className="img-prop" src="https://seeklogo.com/images/T/twitter-new-logo-8A0C4E0C58-seeklogo.com.png?v=638258088440000000" alt="" /></center>
+      <center><img className="img-prop" id="insta-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png?20200512141346" alt="" /></center>
+      <center><img  className="img-prop" id="twitter-img" src="https://seeklogo.com/images/T/twitter-new-logo-8A0C4E0C58-seeklogo.com.png?v=638258088440000000" alt="" /></center>
     </div>
     <div className="nucleus2">
-    <center><img className="img-prop" src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png" alt="" /></center>
-      <center><img  className="img-prop" src="https://cdn4.iconfinder.com/data/icons/social-media-2210/24/Medium-512.png" alt="" /></center>
+    <center><img className="img-prop" id="git-img" onClick={console.log("GIT")} src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_1280.png" alt="" /></center>
+    <center><img  className="img-prop" id="med-img" src="https://cdn4.iconfinder.com/data/icons/social-media-2210/24/Medium-512.png" alt="" /></center>
     </div>
     <div className="nucleus3">
-    <center><img className="img-prop" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" /></center>
-      <center><img  className="img-prop" src="https://static-00.iconduck.com/assets.00/gmail-icon-256x256-lp4z4kz6.png" alt="" /></center>
+    <center><img className="img-prop" id="link-img" src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" /></center>
+      <center><img  className="img-prop" id="mail-img" src="https://static-00.iconduck.com/assets.00/gmail-icon-256x256-lp4z4kz6.png" alt="" /></center>
     </div>
     <div className="nucleus4">
-    <center><img className="img-prop" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png" alt="" /></center>
-      <center><img  className="img-prop" src="https://media.licdn.com/dms/image/C4D03AQFj1UEp1hibQA/profile-displayphoto-shrink_200_200/0/1662981447177?e=1701907200&v=beta&t=nAQkppRVV8iJqW0zZU8g9WigAa04eZh58z0lNY1GJzI" alt="" /></center>
+    <center><img className="img-prop" id="what-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png" alt="" /></center>
+      <center><img  className="img-prop" id="sus-img" src="https://media.licdn.com/dms/image/C4D03AQFj1UEp1hibQA/profile-displayphoto-shrink_200_200/0/1662981447177?e=1701907200&v=beta&t=nAQkppRVV8iJqW0zZU8g9WigAa04eZh58z0lNY1GJzI" alt="" /></center>
     </div>
+    <div className="center"></div>
     </div>
    </div>
-   <div className="center"></div>
+   
     </div>
   )
 }
