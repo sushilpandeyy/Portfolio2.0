b28f3754d42dc07523b53d1410c194e1f696678d
Contactsushil <contact.sushilpandey@gmail.com>
2023-10-15 12:48:36 +0530

    Added Dark Mode Funtionality

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import reactsvg from '../assets/react.svg'
 import Night from '../assets/night.png'
+import Day from "../assets/day.png"
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+import {change} from "../feature/darkmode"
+
 const Header = () => {
+  const currentMode = useSelector((state) => state.mode.current);
+  const dispatch = useDispatch();
+  const [curr, setcurr] =useState(currentMode); 
+  
+  const setMode = () => {
+    const newMode = currentMode === 'Light' ? 'Dark' : 'Light';
+    dispatch(change({ mode: newMode }));
+    setcurr(newMode);
+  };
+
   return (
     <>
-    <div className="header">
+    <div className="header" style={{
+         backgroundColor: (curr === "Light") ? "white" : "#1B1F23"
+      }}>
         <div className="logo">
             <a href="https://contactsushil.vercel.app/"><img src={reactsvg}></img></a>
+            {console.log(currentMode)}
         </div>
-        <div className="menu"><img className='dark-bright' src={Night} alt=""/></div>
+        <div className="menu"><img className='dark-bright' src={(curr=="Light")?Night:Day} onClick={setMode} alt=""/></div>
     </div>
     </>
   )
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import modeReducer from "../feature/darkmode";
+
+const store = configureStore({
+  reducer: {
+    mode: modeReducer,
+    
+  },
+});
+
+export default store;
