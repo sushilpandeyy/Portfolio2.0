01614a5879cfe33c8c930f855190c86383e008ea
Contactsushil <contact.sushilpandey@gmail.com>
2023-12-29 09:54:20 +0530

    Updated Resume
    Updated Resume

--- data/index.js
+++ data/index.js
@@ -4,7 +4,7 @@ const data = [{
     type: "Resume",
     image: "https://static.vecteezy.com/system/resources/previews/003/464/704/original/resume-writing-concept-businessman-new-career-path-illustration-vector.jpg",
     info: "Click to check my  Resume",
-    link: "https://drive.google.com/file/d/1kMkrptWTRhco4sMppkAIUMbeEiYHZak-/view"
+    link: "https://drive.google.com/file/d/1sVRUub3c4bdcIAI2RO0lbgshXeOCYt2u/view?usp=sharing"
 },
 {
     id: "2",
