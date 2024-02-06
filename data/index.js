f0c211cdab6e1cd8a2bb4c01c405f351c897b4c2
Contactsushil <contact.sushilpandey@gmail.com>
2024-02-06 11:44:49 +0530

    Changed Gdrive to aws

--- data/index.js
+++ data/index.js
@@ -4,7 +4,7 @@ const data = [{
     type: "Resume",
     image: "https://static.vecteezy.com/system/resources/previews/003/464/704/original/resume-writing-concept-businessman-new-career-path-illustration-vector.jpg",
     info: "Click to check my  Resume",
-    link: "https://drive.google.com/file/d/1tCxii34fR_pDwdLgWTyqcHu4s3vvW9hR/view?usp=sharing"
+    link: "https://ihhplayer.s3.ap-south-1.amazonaws.com/Sushil+Pandey+Resume3.5.pdf"
 },
 {
     id: "2",
